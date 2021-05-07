3ab8c1866ed6de251b67c19136157a8a4944d4b6
jangjunhyeok <95jjangjun@gmail.com>
2021-05-07 11:45:33 +0900

    Update UserController, HouseController

--- src/main/java/com/ssafy/happyhouse/controller/HouseController.java
+++ src/main/java/com/ssafy/happyhouse/controller/HouseController.java
@@ -1,5 +1,16 @@
 package com.ssafy.happyhouse.controller;
 
-public class HouseController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/house")
+public class HouseController {
+	
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public String regist() {
+		return "house/houseRegist";
+	}
 }
--- src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -1,5 +1,35 @@
 package com.ssafy.happyhouse.controller;
 
-public class UserController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
+	public String mypage() {
+		return "user/mypage";
+	}
+	
 }
