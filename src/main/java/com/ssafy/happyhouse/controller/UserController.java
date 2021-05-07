5e2f714a686d96c97fd97ea4ff5c9ccbaaa64ce6
이종현 <dlwhdgus53@naver.com>
2021-05-07 15:34:35 +0900

    msg commit

--- src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,14 +27,15 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(MemberDto member, HttpSession session) {
+	public String login(MemberDto member, HttpSession session, Model model) {
 		
 		MemberDto loginInfo = mservice.login(member);
 		if (loginInfo != null) {
+			model.addAttribute("msg", "로그인 성공했습니다.");
 			session.setAttribute("userinfo", loginInfo);
 			return "index";
 		}else {
-			
+			model.addAttribute("msg", "로그인 실패했습니다.");
 			return "user/login";
 		}
 	}
