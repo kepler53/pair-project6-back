80ff390f44ff237c070bd98306638146acf425d6
이종현 <dlwhdgus53@naver.com>
2021-05-18 16:56:10 +0900

    cross origin

--- src/main/java/com/ssafy/happyhouse/controller/BoardController.java
+++ src/main/java/com/ssafy/happyhouse/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.happyhouse.dto.BoardDto;
 import com.ssafy.happyhouse.service.BoardService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/board")
 public class BoardController {
@@ -73,11 +75,16 @@ public class BoardController {
 		}
 	}
 	
-//	@GetMapping("/{board_no}")
-//	public BoardDto selectBoard(@PathVariable("board_no") String num) {
-//		
-//		
-//		
-//	}
+	@GetMapping("/{board_no}")
+	public BoardDto selectBoard(@PathVariable("board_no") String num) {
+		
+		BoardDto board = bservice.selectBoard(num);
+		
+		if(board==null) {
+			return null;
+		}else {
+			return board;
+		}
+	}
 	
 }
